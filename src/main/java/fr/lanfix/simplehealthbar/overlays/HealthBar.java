eab4874255cb98cd0b11f6da34159786753229e6
Lanfix <raphael.chapotard@outlook.fr>
2023-06-28 20:05:30 +0200

    [v1.0.2 - Update] Updated to 1.20

--- src/main/java/fr/lanfix/simplehealthbar/overlays/HealthBar.java
+++ src/main/java/fr/lanfix/simplehealthbar/overlays/HealthBar.java
@@ -1,12 +1,9 @@
 package fr.lanfix.simplehealthbar.overlays;
 
-import com.mojang.blaze3d.systems.RenderSystem;
-import com.mojang.blaze3d.vertex.PoseStack;
 import fr.lanfix.simplehealthbar.SimpleHealthBar;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.Font;
-import net.minecraft.client.gui.GuiComponent;
-import net.minecraft.client.renderer.GameRenderer;
+import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.entity.player.Player;
@@ -29,19 +26,19 @@ public class HealthBar implements IGuiOverlay {
     private float intermediateHealth = 0;
 
     @Override
-    public void render(ForgeGui gui, PoseStack poseStack, float partialTick, int width, int height) {
+    public void render(ForgeGui gui, GuiGraphics guiGraphics, float partialTick, int width, int height) {
         if (gui.shouldDrawSurvivalElements()) {
             Font font = gui.getFont();
             Player player = (Player) Minecraft.getInstance().cameraEntity;
             if (player == null) return;
-            float x = (float) width / 2 - 91;
-            float y = height - 39;
+            int x = width / 2 - 91;
+            int y = height - 39;
             updateBarTextures(player);
-            renderHealthValue(font, poseStack, x, y, player);
-            renderHealthBar(poseStack, partialTick, x, y, player);
+            renderHealthValue(font, guiGraphics, x, y, player);
+            renderHealthBar(guiGraphics, partialTick, x, y, player);
             if (player.getAbsorptionAmount() > 0) {
-                renderAbsorptionValue(font, poseStack, x, y, player);
-                renderAbsorptionBar(poseStack, x, y, player);
+                renderAbsorptionValue(font, guiGraphics, x, y, player);
+                renderAbsorptionBar(guiGraphics, x, y, player);
             }
         }
     }
@@ -58,16 +55,14 @@ public class HealthBar implements IGuiOverlay {
         }
     }
 
-    private void renderHealthValue(Font font, PoseStack poseStack, float x, float y, Player player) {
+    private void renderHealthValue(Font font, GuiGraphics guiGraphics, int x, int y, Player player) {
         double health = Math.ceil(player.getHealth() * 10) / 10;
         String text = health + "/" + (int) player.getMaxHealth();
         text = text.replace(".0", "");
-        font.draw(poseStack, text, x - font.width(text) - 6, y + 1, 0xFF0000);
+        guiGraphics.drawString(font, text, x - font.width(text) - 6, y + 1, 0xFF0000);
     }
 
-    private void renderHealthBar(PoseStack poseStack, float partialTick, float x, float y, Player player) {
-        RenderSystem.setShader(GameRenderer::getPositionTexShader);
-        RenderSystem.setShaderColor(1.0F, 1.0F, 1.0F, 1.0F);
+    private void renderHealthBar(GuiGraphics guiGraphics, float partialTick, int x, int y, Player player) {
         float health = player.getHealth();
         float maxHealth = player.getMaxHealth();
         // Calculate bar proportions
@@ -85,23 +80,22 @@ public class HealthBar implements IGuiOverlay {
         int healthWidth = (int) Math.ceil(80 * healthProportion);
         int intermediateWidth = (int) Math.ceil(80 * intermediateProportion);
         // Display full part
-        RenderSystem.setShaderTexture(0, currentBarLocation);
-        GuiComponent.blit(poseStack,
-                (int) x, (int) y,
+        guiGraphics.blit(currentBarLocation,
+                x, y,
                 0, 0,
                 healthWidth, 9,
                 80, 9);
         // Display intermediate part
-        RenderSystem.setShaderTexture(0, intermediateHealthBarLocation);
-        GuiComponent.blit(poseStack,
-                (int) x + healthWidth, (int) y,
+        guiGraphics.blit(intermediateHealthBarLocation,
+                x, y,
+                x + healthWidth, y,
                 healthWidth, 0,
                 intermediateWidth, 9,
                 80, 9);
         // Display empty part
-        RenderSystem.setShaderTexture(0, emptyHealthBarLocation);
-        GuiComponent.blit(poseStack,
-                (int) x + healthWidth + intermediateWidth, (int) y,
+        guiGraphics.blit(emptyHealthBarLocation,
+                x, y,
+                x + healthWidth + intermediateWidth, y,
                 healthWidth + intermediateWidth, 0,
                 80 - healthWidth - intermediateWidth, 9,
                 80, 9);
@@ -112,33 +106,28 @@ public class HealthBar implements IGuiOverlay {
         }
     }
 
-    private void renderAbsorptionValue(Font font, PoseStack poseStack, float x, float y, Player player) {
+    private void renderAbsorptionValue(Font font, GuiGraphics guiGraphics, int x, int y, Player player) {
         double absorption = Math.ceil(player.getAbsorptionAmount());
         String text = String.valueOf(absorption / 2);
         text = text.replace(".0", "");
-        font.draw(poseStack, text, x - font.width(text) - 16, y - 9, 0xFFFF00);
-        RenderSystem.setShader(GameRenderer::getPositionTexShader);
-        RenderSystem.setShaderColor(1.0F, 1.0F, 1.0F, 1.0F);
-        RenderSystem.setShaderTexture(0, guiIconsLocation);
+        guiGraphics.drawString(font, text, x - font.width(text) - 16, y - 9, 0xFFFF00);
         // blit heart container
-        GuiComponent.blit(poseStack,
-                (int) x - 16, (int) y - 10,
+        guiGraphics.blit(guiIconsLocation,
+                x - 16, y - 10,
                 16, 0,
                 9, 9,
                 256, 256);
         // blit heart
-        RenderSystem.setShaderColor(255.0F, 255.0F, 0.0F, 0.0F);
-        GuiComponent.blit(poseStack,
-                (int) x - 16, (int) y - 10,
+        guiGraphics.setColor(127F, 127F, 0F, 0.5F);
+        guiGraphics.blit(guiIconsLocation,
+                x - 16, y - 10,
                 160, 0,
                 9, 9,
                 256, 256);
-        RenderSystem.setShaderColor(1.0F, 1.0F, 1.0F, 1.0F);
+        guiGraphics.setColor(0F, 0F, 0F, 0F);
     }
 
-    private void renderAbsorptionBar(PoseStack poseStack, float x, float y, Player player) {
-        RenderSystem.setShader(GameRenderer::getPositionTexShader);
-        RenderSystem.setShaderColor(1.0F, 1.0F, 1.0F, 1.0F);
+    private void renderAbsorptionBar(GuiGraphics guiGraphics, int x, int y, Player player) {
         float absorption = player.getAbsorptionAmount();
         float maxHealth = player.getMaxHealth();
         // Calculate bar proportions
@@ -146,16 +135,14 @@ public class HealthBar implements IGuiOverlay {
         if (absorptionProportion > 1) absorptionProportion = 1F;
         int absorptionWidth = (int) Math.ceil(80 * absorptionProportion);
         // Display full part
-        RenderSystem.setShaderTexture(0, absorptionBarLocation);
-        GuiComponent.blit(poseStack,
-                (int) x, (int) y - 10,
+        guiGraphics.blit(absorptionBarLocation,
+                x, y - 10,
                 0, 0,
                 absorptionWidth, 9,
                 80, 9);
         // Display empty part
-        RenderSystem.setShaderTexture(0, emptyHealthBarLocation);
-        GuiComponent.blit(poseStack,
-                (int) x + absorptionWidth, (int) y - 10,
+        guiGraphics.blit(emptyHealthBarLocation,
+                x + absorptionWidth, y - 10,
                 absorptionWidth, 0,
                 80 - absorptionWidth, 9,
                 80, 9);
